64f6206f0d3770d2cc44c661dbafe101fcb5883f
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-23 08:39:23 +0100

    Makefile using all target

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
+all: blah
+
 blah: blah.o
 	gcc blah.o -o blah
 
 blah.o: blah.c
 	gcc -c blah.c
+
+clean:
+	rm -f *.o
--- blah.c
+++ blah.c
@@ -2,14 +2,14 @@
 
 /**
  * main - entry point to the programme that count
- * to 10.
+ * to 20.
  * Return: 0 on Success
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 1; i <= 20; i++)
 		printf("%d\n", i);
 	return (0);
 }
